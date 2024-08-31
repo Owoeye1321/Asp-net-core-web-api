fb62b01b4845d666adc8a90ad35ddda0ed60daf5
Owoeye Samuel <owoeye1321@gmail.com>
2024-09-01 00:34:18 +0100

    ohy love this development with ASP; 🚀

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetApi.Controllers
+{
+    //https://localhost:7068/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET:https://localhost:7068/api/students
+        [HttpGet()]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "john", "Jane", "Mark" };
+            return Ok(new { code = 200, students = studentNames });
+        }
+        
+    }
+}
---
+++ Models/Domain/Difficulty.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+namespace dotnetApi.Models.Domain
+{
+	public class Difficulty
+	{
+		public Guid Id { get; set; }
+		public String Name { get; set; }
+	}
+}
+
